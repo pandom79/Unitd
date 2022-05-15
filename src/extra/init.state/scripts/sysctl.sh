a960477cc064d3c9cd6e162aa692111cc9552ae8
Domenico Panella <pandom79@gmail.com>
2022-05-15 18:36:37 +0200

    Changed sysctl.sh init script

--- src/extra/init.state/scripts/sysctl.sh
+++ src/extra/init.state/scripts/sysctl.sh
@@ -1,10 +1,6 @@
 #!/bin/sh -e
 
 # Configure kernel parameters:
-if [ -x /sbin/sysctl -a -r /etc/sysctl.conf -a -z "$VIRTUALIZATION" ]; then
-  /sbin/sysctl -e --system || true
-elif [ -x /sbin/sysctl -a -z "$VIRTUALIZATION" ]; then
-  # Don't say "Applying /etc/sysctl.conf" or complain if the file doesn't exist
-  /sbin/sysctl -e --system 2> /dev/null || true
+if [ -x /sbin/sysctl -a -z "$VIRTUALIZATION" ]; then
+  /sbin/sysctl -e --system > /dev/null || true
 fi
-
