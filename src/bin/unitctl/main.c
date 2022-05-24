8843be78aac9a196e348e48b3ca2d09eb9b511db
Domenico Panella <pandom79@gmail.com>
2022-05-24 11:37:13 +0200

    Fixed unitctl no command case

--- src/bin/unitctl/main.c
+++ src/bin/unitctl/main.c
@@ -111,7 +111,8 @@ int main(int argc, char **argv) {
     /* Command handling */
     switch (command) {
         case NO_COMMAND:
-            if (argc > 3 && (!UNITCTL_DEBUG && !onlyWtmp))
+            if ((argc >= 3 && UNITCTL_DEBUG && onlyWtmp) ||
+                (argc > 2 && (!UNITCTL_DEBUG || !onlyWtmp)))
                 usage(true);
             else {
                 if (onlyWtmp) {
