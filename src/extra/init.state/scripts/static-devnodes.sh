f896916d2f2e99c0c5b7bff22ceb209332df9c0a
Domenico Panella <pandom79@gmail.com>
2023-05-15 10:20:05 +0200

    Updated functions and scripts by void runit.

--- src/extra/init.state/scripts/static-devnodes.sh
+++ src/extra/init.state/scripts/static-devnodes.sh
@@ -5,6 +5,6 @@ export PATH=$PATH
 # Some kernel modules must be loaded before starting udev(7).
 # Load them by looking at the output of `kmod static-nodes`.
 
-for f in $(kmod static-nodes 2>/dev/null|awk '/Module/ {print $2}'); do
-	modprobe -bq $f 2>/dev/null
+for f in $(kmod static-nodes -f devname 2>/dev/null|cut -d' ' -f1); do
+    modprobe -bq $f 2>/dev/null
 done
